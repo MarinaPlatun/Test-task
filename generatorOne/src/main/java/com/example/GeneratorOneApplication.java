80a35e2869bb45985705ca67c6ba748dfbc0182c
Marina Platun <m.platun@fil-it.ru>
2026-01-08 17:58:30 +0300

    fix[kafka]: fix kafka config

--- generatorOne/src/main/java/com/example/GeneratorOneApplication.java
+++ generatorOne/src/main/java/com/example/GeneratorOneApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableKafka
 public class GeneratorOneApplication {
 
 	public static void main(String[] args) {
